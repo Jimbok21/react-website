b0b8ad1bdb96b01fa34e87a89185cf53723bb186
Jimbok21 <jimmykells@sky.com>
2022-06-27 12:42:06 +0100

    added basic modal with no content

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,16 +3,21 @@ import projects from './projects.css'
 // import Modal from '../modal/Modal'
 //imports the database of projects for the cards
 import { data } from '../../assets/projects_data.js'
-import { useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
+
+import Modal from "react-modal";
 
 
-
+Modal.setAppElement("#root");
 
 //javascript to turn data array into cards dynamically
 const Projects = () => {
+
   const [isOpen, setIsOpen] = useState(false)
 
-
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
 
   useEffect(() => {
     if (isOpen) {
@@ -21,51 +26,68 @@ const Projects = () => {
   }, [isOpen])
 
   return (
-    <section id='projects'>
-      <div className='titles'>
-        <h5>My Work</h5>
-        <h2 theme={projects}>Projects</h2>
-      </div>
-      <button onClick={() => {setIsOpen(true)}}>Trigger Modal</button>
+    <>
 
-      <div class="card-grid">
-      {/* <Modal open={isOpen} onClose={() => setIsOpen(false)}>{ID}</Modal> */}
-      
-        {
-          data.map(({ id, image, title, description, date, status, category, difficulty, pattern }) => {
-            return (
-              
-              //add href to new page when theyre created
-              // <div class="card" key={id} onClick={() => {setIsOpen(true); setID(id)}}>
-              <div class="card" key={id}>
-                <img src={image} alt={title} />
-                <div class={pattern}>
-                  <div class="card-text">
-                    <span class="date">{date}</span>
-                    <h2>{title}</h2>
-                    <p>{description}</p>
+
+      <section id='projects'>
+        <div className='titles'>
+          <h5>My Work</h5>
+          <h2 theme={projects}>Projects</h2>
+        </div>
+        {/* <button onClick={() => { setIsOpen(true) }}>Trigger Modal</button> */}
+
+        <div class="card-grid">
+          {
+            data.map(({ id, image, title, description, date, status, category, difficulty, pattern }) => {
+              return (
+
+                //add href to new page when theyre created
+                // <div class="card" key={id} onClick={() => { setIsOpen(true); setID(id) }}>
+                <>
+                  <Modal
+                    isOpen={isOpen}
+                    onRequestClose={toggleModal}
+                    contentLabel="My dialog"
+                    className="mymodal"
+                    overlayClassName="myoverlay"
+                    closeTimeoutMS={500}
+                  >
+                    <div>My modal dialog.</div>
+                    <button onClick={toggleModal}>Close modal</button>
+                  </Modal>
+                  <div class="card" key={id} onClick={() => { setIsOpen(true) }}>
+
+                    <img src={image} alt={title} />
+                    <div class={pattern}>
+                      <div class="card-text">
+                        <span class="date">{date}</span>
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                      </div>
+                    </div>
+                    <div class="card-stats">
+                      <div class="stat">
+                        <div class="value">{status}</div>
+                        <div class="type">status</div>
+                      </div>
+                      <div class="stat border">
+                        <div class="value">{category}</div>
+                        <div class="type">category</div>
+                      </div>
+                      <div class="stat">
+                        <div class="value">{difficulty}</div>
+                        <div class="type">difficulty</div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div class="card-stats">
-                  <div class="stat">
-                    <div class="value">{status}</div>
-                    <div class="type">status</div>
-                  </div>
-                  <div class="stat border">
-                    <div class="value">{category}</div>
-                    <div class="type">category</div>
-                  </div>
-                  <div class="stat">
-                    <div class="value">{difficulty}</div>
-                    <div class="type">difficulty</div>
-                  </div>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
-    </section>
+                </>
+              )
+            })
+
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
